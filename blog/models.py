d2c7261fc9e485c36cdc2ab8814211f842674246
Maria Grozova <marija.grozova@gmail.com>
2024-07-25 13:11:05 +0000

    create Comment model

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,15 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+    Post, on_delete= models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
